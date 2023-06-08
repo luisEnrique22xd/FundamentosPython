3dba46bec419e1273ce27700434789e12d2cec43
Luis Enrique <luiskike2210@gmail.com>
2023-06-08 15:56:08 -0600

    01 Funcion print

---
+++ main.py
@@ -0,0 +1,7 @@
+#function print
+print ("Hola mundo")
+#podemos enviar distintos tipos de datos 
+print ("un String")
+print ("un entero", 25 )
+print ("un float", 3.1416)
+print ("un boolean", True)
