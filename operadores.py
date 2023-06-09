d88bdfe219e36297ae50e477554207ae2b9a3650
Luis Enrique <luiskike2210@gmail.com>
2023-06-09 14:00:04 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+#Operadores Matematicos
+# + - * %
+# / Division float
+# // Division entera 
+# ** Potencia 
+
+print (2+2)
+print(2-2)
+print(2*2)
+print (2%2)
+print (10/3)
+print (10//3)
+print (3**3)
+
+#operadores para String
+# + (contatenar) * (replica)
+print("hola "+ "Mundo!")
+print("hola \n"*4)
