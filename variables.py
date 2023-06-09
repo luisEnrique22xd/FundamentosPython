30498f7f7ad2e4e299b339f4dd21b273c82c411e
Luis Enrique <luiskike2210@gmail.com>
2023-06-09 13:24:10 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+#Python es no Tipado
+name = "Luis Sanchez"
+age= 18 
+height =1.68
+is_student= True 
+children=[]
+
+print(type(name))
+print(type(age))
+print (type(height))
+print(type(is_student))
+print(type(children))
