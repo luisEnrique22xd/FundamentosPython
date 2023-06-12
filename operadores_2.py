87f07dbbf6b97d9363d0f8efeaaffbb23eea3475
Luis Enrique <luiskike2210@gmail.com>
2023-06-12 09:53:02 -0600

    05 Operadores Logicos y Booleanos

---
+++ operadores_2.py
@@ -0,0 +1,25 @@
+#operadores booleanos
+# > >=
+print(10>10)
+print(10>=10)
+# < <=
+print(10<10)
+print(10<=10)
+# ==
+print(10==10)
+# != 
+print(10!=10)
+
+
+#operadores logicos 
+# and (True if all true)
+print(True and True)
+print(True and False)
+# or (True if all least one true)
+print(True or True)
+print(True or False)
+# not (not True )
+print(not True)
+print(not False)
+
+
