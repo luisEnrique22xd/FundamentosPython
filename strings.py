87bbd3819dc4f2a358e0753af1a6231f94d7a204
Luis Enrique <luiskike2210@gmail.com>
2023-06-13 14:16:32 -0600

    07 Strings

---
+++ strings.py
@@ -0,0 +1,20 @@
+my_string = "H0la mUnDo"
+
+#Mayuscula
+print (my_string.upper())
+#Minusculas 
+print (my_string.lower())
+#logitud de String
+print(len(my_string))
+# Quitar espacios inicio y fin 
+print(my_string.strip)
+
+my_string = "Este es un nuevo texto"
+
+# Index and slicing 
+print(my_string[0])
+print(my_string[3])
+print(my_string[6])
+
+print(my_string[-1])
+print(my_string[-2])
