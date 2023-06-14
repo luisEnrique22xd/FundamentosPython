e7fd0086457073465fd3560e5e0033f4a08c2628
Luis Enrique <luiskike2210@gmail.com>
2023-06-14 11:27:38 -0600

    09 Tuplas y Conjutos

---
+++ colecciones-2.py
@@ -0,0 +1,16 @@
+#tuplas
+my_tuple = ('Hola', 12, 1.5, True)
+print (type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[-1])
+#error
+#my_tuple[0] ="otro valor"
+
+#conjuntos (set)
+my_set = {1, 2, 4, 4, 4, 4, 4, True, "Hi bitch"}
+print (type(my_set))
+print(my_set)
+
+lista = [1, 2, 3, 1, 2, 4, 3, 5, 6]
+print(lista)
+print(set(lista))
